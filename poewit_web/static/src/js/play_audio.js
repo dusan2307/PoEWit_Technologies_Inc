58639ca923694461c9fd4c23e37310fc0c97bdb7
sna-odoo <sna@odoo.com>
2020-08-04 09:25:12 -0700

    [ADD] Web Modifications

--- poewit_web/static/src/js/play_audio.js
+++ poewit_web/static/src/js/play_audio.js
@@ -3,3 +3,10 @@ function playAudio(){
     var sound = new Audio(audio);
     sound.play();
 }
+
+(function (){
+    var anchor=document.querySelectorAll("[data-mimetype='application/pdf']");
+    for(var i = 0; i < anchor.length; i++){ 
+        anchor[i].innerHTML="Download PDF";
+    };
+})();
