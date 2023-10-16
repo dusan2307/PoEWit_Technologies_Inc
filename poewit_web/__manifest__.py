2b7430910822f6c73a77bebf02ec8439ea5cd34c
yuro-odoo <108021115+yuro-odoo@users.noreply.github.com>
2023-10-16 09:24:26 -0700

    15.0 upgrade (#13)
    * [FIX] upgraded code to compatible with Odoo15
    
    * [FIX]fixed external_id not found error: removed website_id

--- poewit_web/__manifest__.py
+++ poewit_web/__manifest__.py
@@ -3,9 +3,9 @@
     'name': 'PoEWit Technologies Inc: Adding a sound file & changing attachment icon on website',
     'summary': 'Customization on the web (sound,download button)',
     'sequence': 100,
-    'license': 'OEEL-1',
+    'license': 'OPL-1',
     'website': 'https://www.odoo.com',
-    'version': '1.1',
+    'version': '1.2',
     'author': 'Odoo Inc',
     'description': """
     -  2286978
@@ -13,12 +13,18 @@
     'category': 'Custom Development',
 
     # any module necessary for this one to work correctly
-    'depends': ['website_sale'],
+    'depends': ['website_sale', 'website_sale_wishlist'],
 
     # always loaded
     'data': [
         'views/poewit_web_views.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'poewit_web/static/src/scss/style.scss',
+            'poewit_web/static/src/js/play_audio.js',
+        ],
+    },
     'installable': True,
     'auto_install': False,
 }
